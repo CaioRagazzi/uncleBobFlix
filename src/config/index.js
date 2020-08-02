f1dbe9f77e47e8fd966e8b5db8bf915f5cb2f103
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-08-02 12:12:47 -0300

    adicionando api heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://devsoutinhoflix.herokuapp.com';
+  : 'https://bobflix.herokuapp.com/';
 
 export default {
   URL_BACKEND,
